c430eb6381070c6f2ac96bff0d46987bcfaf06aa
Helekin <clsjs94@hotmail.com>
2023-09-22 16:47:22 -0500

    shipping

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,11 +9,13 @@ import {
 import { Provider } from "react-redux";
 
 import App from "./App.jsx";
+import PrivateRoute from "./components/PrivateRoute.jsx";
 import HomeScreen from "./screens/HomeScreen.jsx";
 import ProductScreen from "./screens/ProductScreen.jsx";
 import CartScreen from "./screens/CartScreen.jsx";
 import LoginScreen from "./screens/LoginScreen.jsx";
 import RegisterScreen from "./screens/RegisterScreen.jsx";
+import ShippingScreen from "./screens/ShippingScreen.jsx";
 
 import store from "./store.js";
 
@@ -28,6 +30,11 @@ const router = createBrowserRouter(
       <Route path="/cart" element={<CartScreen />} />
       <Route path="/login" element={<LoginScreen />} />
       <Route path="/register" element={<RegisterScreen />} />
+      <Route path="/shipping" element={<ShippingScreen />} />
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/shipping" element={<ShippingScreen />} />
+      </Route>
     </Route>
   )
 );
